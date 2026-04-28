2c4f1bd9edaa0b3cb2c417c9415b23873cfa3aad
MostafaRamadan <MostafaRamadan@DESKTOP-OD1THEG>
2026-04-28 10:41:24 +0300

    BookStoreDb

---
+++ G-NET-27-EFCore-01/Class/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_NET_27_EFCore_01.Class
+{
+    public class Author
+    {
+        public int id { get; set; }
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Biography { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
